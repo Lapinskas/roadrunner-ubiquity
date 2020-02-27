89406a10a893a424232ecbc811fe3789cf7be0eb
Ernest Igwe <ernest.beson.igwe@gmail.com>
2020-02-27 17:03:56 +0100

    added post-install-cmd script in composer.json and worker.php ubiquity path fixes

--- sample/worker.php
+++ sample/worker.php
@@ -4,16 +4,16 @@ use RoadRunnerUbiquity\Request;
 use Ubiquity\controllers\Startup;
 
 define('DS', DIRECTORY_SEPARATOR);
-define('ROOT', __DIR__.DS.'app'.DS);
+define('ROOT', __DIR__.DS.'..'.DS);
 
 ini_set('display_errors', 'stderr');
 ini_set('max_execution_time', 0);
 
-$config=include_once ROOT.'config/config.php';
+$config=include_once ROOT.'app/config/config.php';
 $config['siteUrl'] = 'http://127.0.0.1:8090/';
 
-require_once ROOT.'./../vendor/autoload.php';
-require_once ROOT.'config/services.php';
+require_once ROOT.'vendor/autoload.php';
+require_once ROOT.'app/config/services.php';
 
 $request = new Request();
 
